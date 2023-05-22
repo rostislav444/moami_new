a16fbff7d1296e583beccba7f0080ddb0578e75f
Rostislav <rostislav444@gmail.com>
2023-05-22 11:57:48 +0300

    mk_photo command added

--- frontend/src/emotion.d.tsx
+++ frontend/src/emotion.d.tsx
@@ -1,16 +1,19 @@
 import '@emotion/react'
 
 declare module '@emotion/react' {
-  export interface Theme {
-    colors: {
-        background: string
+    export interface Theme {
+        colors: {
+            background: string;
+        };
+        color: {
+            primary: string;
+            primaryDark: string;
+            primaryLight: string;
+            grey: string;
+            positive: string;
+            negative: string;
+            error: string;
+        },
     }
-    color: {
-      primary: string
-      grey: string
-      positive: string
-      negative: string
-    }
-  }
 }
 
