9871b6bfb5ba12902af9d59525048d2b80ff822d
Rostislav <rostislav444@gmail.com>
2023-06-01 00:10:45 +0300

    Added api root to urls

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -3,11 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-urlpatterns = [
-    # Admin
-    path('admin/', admin.site.urls),
-    # Apps
+api = [
     path('order/', include('apps.order.urls')),
     path('category/', include('apps.categories.urls')),
     path('catalogue/', include('apps.catalogue.urls')),
@@ -16,5 +12,13 @@ urlpatterns = [
     path('newpost/', include('apps.newpost.urls')),
     path('sizes/', include('apps.sizes.urls')),
     path('', include('apps.core.urls')),
+]
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+    # Apps
+    path('api/', include(api)),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
