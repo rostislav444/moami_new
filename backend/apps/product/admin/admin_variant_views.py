bf3f1d0c97d88d11631be9ef715f75013452b86f
Rostislav <rostislav444@gmail.com>
2023-10-12 23:13:47 +0300

    Updated views admin, added sources

--- backend/apps/product/admin/admin_variant_views.py
+++ backend/apps/product/admin/admin_variant_views.py
@@ -11,6 +11,14 @@ class VarintViewSourceInline(admin.TabularInline):
 
 @admin.register(VariantViews)
 class VariantViewsAdmin(admin.ModelAdmin):
+    def get_source(self, obj):
+        data = []
+        for source in obj.view_sources.all():
+            data.append(source.utm_source + ' - ' + str(source.views))
+        return ', '.join(data)
+
+    get_source.short_description = 'Источники'
+
     def get_code(self, obj):
         return obj.variant.code
 
@@ -24,9 +32,9 @@ class VariantViewsAdmin(admin.ModelAdmin):
 
     get_image.short_description = 'Изображение'
 
-    list_display = ('get_image', 'get_code', 'views', 'day')
-    list_filter = ('day',)
+    list_display = ('get_image', 'get_code', 'views', 'get_source', 'day')
+    list_filter = ('day', 'view_sources__utm_source')
     search_fields = ('variant', 'day')
-    ordering = ('-day', '-views',)
+    ordering = ('-day', '-views', 'view_sources__views')
     readonly_fields = ('get_image', 'get_code', 'variant', 'views', 'day')
     inlines = (VarintViewSourceInline,)
