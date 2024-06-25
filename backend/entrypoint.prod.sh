1f586e2e4a77ddd9be15049bec63545dc8426c88
Rostislav <rostislav444@gmail.com>
2024-06-25 09:45:42 +0300

    Docker deployment

---
+++ backend/entrypoint.prod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate
+
+# change owner of celerybeat-schedule file to app user
+chown app:app /home/app/web/celerybeat-schedule
+
+exec "$@"
