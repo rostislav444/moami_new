8a3f41b630905f2594614891f0e5b5016e216768
Rostislav <rostislav444@gmail.com>
2024-06-26 10:24:22 +0300

    nginx deploy

--- sh/restore_local_db.sh
+++ sh/restore_local_db.sh
@@ -10,8 +10,8 @@ EOF
 
 # Import the dump into your database
 if [[ $(file --mime-type -b ./backend/data/dumps/moami_dump.sql) == "text/plain" ]]; then
-    cat ./backend/data/dumps/moami_dump.sql | docker exec -i moami-db-1 psql -U postgres -d moami_db
+    cat ./backend/data/dumps/moami_dump.sql | docker exec -i moami-db-1 psql -U postgres -d moami
 else
-    docker exec -i moami-db-1 pg_restore -U postgres -d moami_db < ./backend/data/dumps/moami_dump.sql
+    docker exec -i moami-db-1 pg_restore -U postgres -d moami < ./backend/data/dumps/moami_dump.sql
 fi
 
