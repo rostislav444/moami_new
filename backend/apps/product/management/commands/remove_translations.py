2baa618ef1cfc58a742ab694137317257ccbd454
rostislav <rostislav444@gmail.com>
2025-06-03 15:10:49 +0300

    remove_translations.py

---
+++ backend/apps/product/management/commands/remove_translations.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from project.settings import LANGUAGES
+from apps.product.models import Product
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        count = len(LANGUAGES)
+        for product in Product.objects.all():
+            if product.translations.count() > count - 1:
+                for lang in LANGUAGES:
+                    if lang[0] != 'ru':
+                        product.translations.filter(language=lang[0]).last().delete()
+                        print('deleted', product.id, lang[0])
+                product.save()
+        self.stdout.write(self.style.SUCCESS('Successfully removed translations'))
